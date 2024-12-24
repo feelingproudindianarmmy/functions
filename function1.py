b4f16e8513a55701a6959618162ee77938c44884
feelingproudindianarmy <celestialh4@gmail.com>
2024-12-24 17:54:54 +0545

    functions

---
+++ function1.py
@@ -0,0 +1,4 @@
+def my_function(name):
+    print("my name is ",name)
+name=str(input("enter your name"))
+my_function(name)
---
+++ function2.py
@@ -0,0 +1,7 @@
+def my_function(name,place,age):
+    print(name,place,age)
+name=str(input("enter any name"))
+place=str(input("enter your place name"))
+age=int(input("enter any age"))
+my_function(name,place,age)
+my_function(name,place,age)
